d29c6d1e4e39f9a933c717134c3e3932e842ad0f
aylane002 <aylanesantos9@gmail.com>
2020-10-16 17:24:49 -0300

    Listando

--- src/main/java/CadastroServlet.java
+++ src/main/java/CadastroServlet.java
@@ -4,18 +4,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/CadastroServlet")
 public class CadastroServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   // protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+   // }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String nome = request.getParameter("nome");
         String email = request.getParameter("email");
         String senha = request.getParameter("senha");
 
-    }
+        response.setContentType("txt/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title> Servlet CadastroServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3> Nome de login " + nome + "Email " + email + "</h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
 
-   // protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    //}
+}
 }
--- src/main/java/ProdutosServlet.java
+++ src/main/java/ProdutosServlet.java
@@ -4,18 +4,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/ProdutosServlet")
 public class ProdutosServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    //}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nome = request.getParameter("nome");
         String id = request.getParameter("id");
         String descricao = request.getParameter("descricao");
         String valor = request.getParameter("valor");
-    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("txt/html");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title> Servlet ProdutosServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3> Nome do Produto " + nome + "Id " + id + "Descrição " + descricao + "Valor " + valor + "</h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 }
